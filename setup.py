20ad24710ce9940eb09ee103ed498a876001e68d
Henri <git@henrici.name>
2020-10-06 22:02:39 +0200

    Partly consider Nodes on Debian; updated Bird config

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setup_kwargs = {
     'name': 'towalink_tlm',
-    'version': '0.1.2',
+    'version': '0.1.4',
     'author': 'The Towalink Project',
     'author_email': 'pypi.tlm@towalink.net',
     'description': 'command line tool for configuring and managing a Towalink installation',
